7b45ff55cc563dd5d3790e8c2d09515caae8797c
Dorishow <jsantosalmeida54@gmail.com>
2022-04-11 16:53:56 -0300

    feat: listar todas transações

--- src/main/java/com/santander/banco811/service/TransacaoService.java
+++ src/main/java/com/santander/banco811/service/TransacaoService.java
@@ -3,6 +3,9 @@ package com.santander.banco811.service;
 import com.santander.banco811.dto.transacao.TransacaoRequest;
 import com.santander.banco811.dto.transacao.TransacaoResponse;
 
+import java.util.List;
+
 public interface TransacaoService {
     TransacaoResponse create(TransacaoRequest transacaoRequest, Integer idContaRemetente);
+    List<TransacaoResponse> findAll();
 }
